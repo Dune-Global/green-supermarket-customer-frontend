be310860e09aacf2f0d40a117b863dfa6be174b1
WasathTheekshana <wasath.vt@gmail.com>
2023-11-23 09:04:05 +0530

    feat: Metadata file

---
+++ src/data/metadata/metadata.ts
@@ -0,0 +1,6 @@
+export const MainData= {
+  title: "Green Supermarket",
+  description: "Green Supermarket | Online Grocery Shopping in Sri Lanka",
+  keywords:
+    "Green Supermarket, Online Grocery Shopping, Grocery Shopping, Online Grocery Shopping in Sri Lanka",
+};
