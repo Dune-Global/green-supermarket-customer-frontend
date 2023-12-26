3ce72d5dc6cac4c541dedc68766b7797cf534ca0
sanuda <sanuda@gmail.com>
2023-12-27 00:45:13 +0530

    refactor: Billing page

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <BestSelling />
       <CountdownSection />
       <Categories />
-      <TestimonialsSection />
+      <TestimonialsSection/>
       
     </div>
   );
