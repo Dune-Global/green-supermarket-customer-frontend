68eb563264feb487e55e9533247fc4cc3dc9c437
Lithara Perera <litharaperera2002@gmail.com>
2023-12-29 02:29:33 +0530

    refactor: Address book page

---
+++ src/data/address-book/address-book.ts
@@ -0,0 +1,64 @@
+const card = [
+    {
+        id: 1,
+        addressName: "Home",
+        firstName: "John",
+        lastName: "Smith",
+        streetAddress: "22/5A, Kumara Mawatha",
+        city: "Homagama",
+        province: "Western",
+        zipcode: "10500",
+        email: "johnsmith@gmail.com",
+        contactNumber: "(+94) 77 1234567"
+    },
+    {
+        id: 2,
+        addressName: "Work",
+        firstName: "John",
+        lastName: "Smith",
+        streetAddress: "22/5A, Kumara Mawatha",
+        city: "Homagama",
+        province: "Western",
+        zipcode: "10500",
+        email: "johnsmith@gmail.com",
+        contactNumber: "(+94) 77 1234567"
+    },
+    {
+        id: 3,
+        addressName: "Home",
+        firstName: "John",
+        lastName: "Smith",
+        streetAddress: "22/5A, Kumara Mawatha",
+        city: "Homagama",
+        province: "Western",
+        zipcode: "10500",
+        email: "johnsmith@gmail.com",
+        contactNumber: "(+94) 77 1234567"
+    },
+    {
+        id: 4,
+        addressName: "Home",
+        firstName: "John",
+        lastName: "Smith",
+        streetAddress: "22/5A, Kumara Mawatha",
+        city: "Homagama",
+        province: "Western",
+        zipcode: "10500",
+        email: "johnsmith@gmail.com",
+        contactNumber: "(+94) 77 1234567"
+    },
+    {
+        id: 4,
+        addressName: "Home",
+        firstName: "John",
+        lastName: "Smith",
+        streetAddress: "22/5A, Kumara Mawatha",
+        city: "Homagama",
+        province: "Western",
+        zipcode: "10500",
+        email: "johnsmith@gmail.com",
+        contactNumber: "(+94) 77 1234567"
+    },
+];
+  
+  export default card;
