52a6094dccd54707a5d4fedb311c7bb4ab9a05bc
sanuda <sanuda@gmail.com>
2023-12-28 18:17:49 +0530

    feat: Address-book

--- src/app/(client)/(pages)/(customer)/address-book/page.tsx
+++ src/app/(client)/(pages)/(customer)/address-book/page.tsx
@@ -42,7 +42,7 @@ export default function AddressBookPage() {
               <div className="flex flex-col pt-4 gap-2">
                 <div>Addresse name</div>
                 <div>
-                  <div className="pt-">
+                  <div className="pb-2">
                     <Input
                       className=" rounded-md border-gray-200/40"
                       placeholder="Your first name"
@@ -52,7 +52,7 @@ export default function AddressBookPage() {
                 </div>
               </div>
               {/* inputs */}
-              <div className="flex flex-col  gap-4">
+              <div className="flex flex-col  gap-2">
                 <div className="flex flex-row gap-4">
                   {" "}
                   {/* input name */}
@@ -134,7 +134,9 @@ export default function AddressBookPage() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-end">
+
+          <div className="flex flex-row justify-end gap-4 ">
+          <div className="flex ">
             <div className="flex w-96 border border-gray-50 rounded-lg p-4 ">
               <div className="flex flex-col gap-1">
                 <div className="font-medium text-base">John Smith</div>
@@ -152,7 +154,28 @@ export default function AddressBookPage() {
               </div>
             </div>
           </div>
+          <div className="flex ">
+            <div className="flex w-96 border border-gray-50 rounded-lg p-4 ">
+              <div className="flex flex-col gap-1">
+                <div className="font-medium text-base">John Smith</div>
+                <div className="text-sm text-gray-200">
+                  22/5A, Kumara Mawatha, Pitipana South, Homagama.
+                </div>
+                <div className="font-medium text-sm">johnsmith@gmail.com</div>
+                <div className="font-medium text-sm">(+94) 77 1234567</div>
+                <div className="pt-5">
+                  <Button >Edit Address</Button>
+                </div>
+              </div>
+              <div>
+                <Trash2  onClick={handleDeleteAddress} />
+              </div>
+            </div>
+          </div>
+          </div>
+
         </div>
+        
         {showConfirmation && (
           <div className=" fixed inset-0 flex items-center justify-center bg-gray-0 z-40">
             <div className="bg-white p-4 border border-gray-50 rounded-lg ">
@@ -162,13 +185,14 @@ export default function AddressBookPage() {
               </div>
               <div className="flex justify-center mt-4">
                 <Button onClick={handleConfirmDelete} className="bg-red-400 hover:bg-red-600">Delete</Button>
-                <Button onClick={handleCancelDelete} className="ml-2">
+                <Button onClick={handleCancelDelete} className="ml-2 bg-gray-0 border-2 border-red-400 text-gray-200 hover:border-red-600 hover:bg-gray-0">
                  Cancel
                 </Button>
               </div>
             </div>
           </div>
         )}
+        
       </Container>
     </ClientOnly>
   );
