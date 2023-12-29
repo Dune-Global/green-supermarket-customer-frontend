320a8287bc0ad2d31db0130c6d61ddac2fa26eec
Lithara Perera <litharaperera2002@gmail.com>
2023-12-29 13:29:40 +0530

    refactor: Address book page

--- src/components/common/layout/side-menu.tsx
+++ src/components/common/layout/side-menu.tsx
@@ -6,14 +6,33 @@ import { LogOut } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { sideMenuItems } from "@/data/side-menu";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog";
+import { useRouter } from "next/navigation";
+
 type Props = {};
 
 const SideMenu = (props: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const pathName = usePathname();
+  const router = useRouter();
 
   const handleClick = () => {
     setIsOpen(!isOpen);
+  };
+
+  const signOut = () => {
+    localStorage.removeItem("jwtToken");
+    router.push("/");
   };
 
   return (
@@ -89,14 +108,35 @@ const SideMenu = (props: Props) => {
               </div>
             </Link>
           ))}
-          <Link href={"/"} className="py-[1px]">
-            <div
-              className={`p-6 flex gap-3 border-l-4 border-gray-0 hover:bg-gray-50 hover:border-gray-50`}
-            >
-              <LogOut size={22} strokeWidth={2} className="text-gray-200" />
-              <h2 className="text-gray-600">Sign Out</h2>
-            </div>
-          </Link>
+          <AlertDialog>
+                  <AlertDialogTrigger
+                    className={`p-6 flex gap-3 border-l-4 border-gray-0 hover:bg-gray-50 hover:border-gray-50`}
+                  >
+                    <LogOut
+                      size={22}
+                      strokeWidth={2}
+                      className="text-gray-200"
+                    />
+                    <h2 className="text-gray-600">Sign Out</h2>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        You will be signed out of the application. You will need
+                        to sign in again to continue using the application.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction onClick={signOut}>
+                        Continue
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
         </div>
       </div>
     </div>
