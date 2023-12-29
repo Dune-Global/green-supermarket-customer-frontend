2a97c1c54641fb0fde7e4e3542d41cd2c6d15b81
Dilantha Wijesinghe <wdilantha66@gmail.com>
2023-12-29 17:15:36 +0530

    refactor: Remove client only

--- src/components/common/layout/navigation.tsx
+++ src/components/common/layout/navigation.tsx
@@ -13,20 +13,18 @@ const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 1024px)");
 
   return (
-    <ClientOnly>
-      <div>
-        <div className="bg-green-0  z-50 top-0 flex items-center justify-center h-[60px] ">
-          <div className="relative mx-auto w-full max-w-[1500px] xl:px-20 md:px-10 sm:px-10 px-6">
-            <div className="flex justify-between items-center">
-              <NavigationMenuCategory />
-              {!isMobile && <NavigationMenuTextDropdown />}
-              {!isMobile && <NavigationMenuAvatar />}
-              {isMobile && <NavigationMenuHamburger />}
-            </div>
+    <div>
+      <div className="bg-green-0  z-50 top-0 flex items-center justify-center h-[60px] ">
+        <div className="relative mx-auto w-full max-w-[1500px] xl:px-20 md:px-10 sm:px-10 px-6">
+          <div className="flex justify-between items-center">
+            <NavigationMenuCategory />
+            {!isMobile && <NavigationMenuTextDropdown />}
+            {!isMobile && <NavigationMenuAvatar />}
+            {isMobile && <NavigationMenuHamburger />}
           </div>
         </div>
       </div>
-    </ClientOnly>
+    </div>
   );
 };
 
--- src/components/common/layout/top-menu.tsx
+++ src/components/common/layout/top-menu.tsx
@@ -11,28 +11,26 @@ import Link from "next/link";
 export default function TopMenu() {
   const isMobile = useMediaQuery("(max-width: 1024px)");
   return (
-    <ClientOnly>
-      <div className="bg-gray-0  z-50 top-0 flex items-center justify-center h-[70px]">
-        <div className="relative mx-auto w-full max-w-[1500px] xl:px-20 md:px-10 sm:px-10 px-6">
-          <div className="flex justify-between w-full items-center">
-            <div>
-              <Link href="/">
-                <BrandIcon size="large" mode="dark" />
-              </Link>
-            </div>
-            <div className="hidden lg:flex">
+    <div className="bg-gray-0  z-50 top-0 flex items-center justify-center h-[70px]">
+      <div className="relative mx-auto w-full max-w-[1500px] xl:px-20 md:px-10 sm:px-10 px-6">
+        <div className="flex justify-between w-full items-center">
+          <div>
+            <Link href="/">
+              <BrandIcon size="large" mode="dark" />
+            </Link>
+          </div>
+          <div className="hidden lg:flex">
+            <SearchBar />
+          </div>
+          <div className="flex gap-6">
+            <div className="lg:hidden">
               <SearchBar />
             </div>
-            <div className="flex gap-6">
-              <div className="lg:hidden">
-                <SearchBar />
-              </div>
-              <Cart />
-              {isMobile && <NavigationMenuAvatar />}
-            </div>
+            <Cart />
+            {isMobile && <NavigationMenuAvatar />}
           </div>
         </div>
       </div>
-    </ClientOnly>
+    </div>
   );
 }
