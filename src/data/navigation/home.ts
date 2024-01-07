a391b964dd7e6c7eaaf40e999af72ea52057c785
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-01-07 15:01:12 +0530

    feat: top deals page and home section links

--- src/data/navigation/home.ts
+++ src/data/navigation/home.ts
@@ -3,37 +3,37 @@ import { INavigationText } from "@/types";
 const home: INavigationText[] = [
   {
     title: "Featured",
-    href: "#featured",
+    href: "/#featured",
     description:
       "Discover premium-quality groceries handpicked for their freshness and exceptional taste.",
   },
   {
     title: "Best Selling",
-    href: "#best-selling",
+    href: "/#best-selling",
     description:
       "Explore our top-rated grocery essentials loved by customers for their quality and value.",
   },
   {
     title: "Deals",
-    href: "#deals",
+    href: "/#deals",
     description:
       "Save big on exclusive offers and discounts, making your grocery shopping experience even better.",
   },
   {
     title: "Categories",
-    href: "#categories",
+    href: "/#categories",
     description:
       "Easily navigate through a wide array of grocery categories, ensuring a seamless and convenient shopping journey.",
   },
   {
     title: "Testimonials",
-    href: "#testimonials",
+    href: "/#testimonials",
     description:
       "Hear what our satisfied customers have to say about their delightful experiences with us.",
   },
   {
     title: "Newsletter",
-    href: "#newsletter",
+    href: "/#newsletter",
     description:
       " Stay updated on the latest deals, new arrivals, and insider tips by subscribing to our newsletter.",
   },
